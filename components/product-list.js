bce73c489fbf9e8fb48965c2756704e08639f893
Doniyor <oripovd1@gmail.com>
2020-06-23 23:30:18 +0500

    design aside filter component

--- components/product-list.js
+++ components/product-list.js
@@ -7,8 +7,52 @@ export default function ProductList({ data }) {
             <aside className="sidebar">
                 <div className="category_wrapper">
                     <div className="sidebar_wrapper">
-                        <div className="inner_wrapper">
-
+                        <div className="outer_wrapper">
+                            <div className="inner_wrapper">
+                                <form>
+                                    <h3>Фильтр</h3>
+                                    <div className="filter_group">
+                                        <h5>Фильтрация по цене</h5>
+                                        <input type="text" name="amount_from" id="amount_from" placeholder="от" />
+                                        <input type="text" name="amount_to" id="amount_to" placeholder="до" />
+                                    </div>
+                                    <div className="filter_group">
+                                        <h5>Бренды</h5>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="sony" id="sony" />
+                                            <label htmlFor="sony">Sony</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="lg" id="lg" />
+                                            <label htmlFor="lg">LG</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="samsung" id="samsung" />
+                                            <label htmlFor="samsung">Samsung</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="haier" id="haier" />
+                                            <label htmlFor="haier">Haier</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="artel" id="artel" />
+                                            <label htmlFor="artel">Artel</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="rosso" id="rosso" />
+                                            <label htmlFor="rosso">Rosso</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="roison" id="roison" />
+                                            <label htmlFor="roison">Roison</label>
+                                        </div>
+                                        <div className="check_box">
+                                            <input type="checkbox" name="hisense" id="hisense" />
+                                            <label htmlFor="hisense">Hisense</label>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
