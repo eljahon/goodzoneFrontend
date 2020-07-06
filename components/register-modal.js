fafc953dbaadc9054b7e23a8ae4b5f8789a02e0a
Doniyor <oripovd1@gmail.com>
2020-07-06 12:13:53 +0500

    redesign product single and confirmation code form

--- components/register-modal.js
+++ components/register-modal.js
@@ -12,6 +12,7 @@ export default function RegisterModal({ closeModal, login }) {
     const router = useRouter();
     const dispatch = useDispatch();
     const [load, setLoad] = useState(false);
+    const [registerConfirm, setRegisterConfirm] = useState(false);
     useEffect(() => {
         setLoad(true);
         document.body.classList.add("overflow");
@@ -62,67 +63,87 @@ export default function RegisterModal({ closeModal, login }) {
             <div className={`login_modal-holder ${load ? "show" : ""}`}>
                 <div className="inner_block">
                     <div className="auth_form">
-                        <div className="auth_form-container">
-                            <h3>Регистрация</h3>
-                            <span className="sub_heading"></span>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                                <input
-                                    ref={register}
-                                    name="firstName"
-                                    placeholder="Имя"
-                                    required
-                                />
-                                <input
-                                    ref={register}
-                                    name="lastName"
-                                    placeholder="Фамилия"
-                                    required
-                                />
-                                <input
-                                    ref={register({
-                                        maxLength: 13,
-                                        minLength: 13,
-                                    })}
-                                    defaultValue="+998"
-                                    type="tel"
-                                    name="phoneNumber"
-                                    placeholder="Номер телефона"
-                                    required
-                                />
-                                {errors.phoneNumber && (
-                                    <p>
-                                        Phone number should be 13 characters
-                                        long
-                                    </p>
-                                )}
-                                <input
-                                    ref={register}
-                                    type="password"
-                                    name="password"
-                                    placeholder="Пароль"
-                                    required
-                                />
-                                <input
-                                    ref={register}
-                                    type="password"
-                                    name="passwordConfirmation"
-                                    placeholder="Подтверждение пароля"
-                                    required
-                                />
-                                <input
-                                    type="submit"
-                                    className="btn btn_submit"
-                                    value="Регистрация"
-                                />
-                            </form>
-                            <p className="auth_form-offer">
-                                <span>Уже есть аккаунт? </span>
-                                <button className="btn" onClick={login}>
-                                    {" "}
+                        {registerConfirm ?
+                            <div className="auth_form-container">
+                                <h3>Подтвердить пароль</h3>
+                                <span className="sub_heading"></span>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <input
+                                        ref={register}
+                                        name="code"
+                                        placeholder="Код"
+                                        required
+                                    />
+                                    <input
+                                        type="submit"
+                                        className="btn btn_submit"
+                                        value="Отправить"
+                                    />
+                                </form>
+                                <p className="auth_form-offer"></p>
+                            </div>
+                            :
+                            <div className="auth_form-container">
+                                <h3>Регистрация</h3>
+                                <span className="sub_heading"></span>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <input
+                                        ref={register}
+                                        name="firstName"
+                                        placeholder="Имя"
+                                        required
+                                    />
+                                    <input
+                                        ref={register}
+                                        name="lastName"
+                                        placeholder="Фамилия"
+                                        required
+                                    />
+                                    <input
+                                        ref={register({
+                                            maxLength: 13,
+                                            minLength: 13,
+                                        })}
+                                        defaultValue="+998"
+                                        type="tel"
+                                        name="phoneNumber"
+                                        placeholder="Номер телефона"
+                                        required
+                                    />
+                                    {errors.phoneNumber && (
+                                        <p>
+                                            Phone number should be 13 characters
+                                            long
+                                        </p>
+                                    )}
+                                    <input
+                                        ref={register}
+                                        type="password"
+                                        name="password"
+                                        placeholder="Пароль"
+                                        required
+                                    />
+                                    <input
+                                        ref={register}
+                                        type="password"
+                                        name="passwordConfirmation"
+                                        placeholder="Подтверждение пароля"
+                                        required
+                                    />
+                                    <input
+                                        type="submit"
+                                        className="btn btn_submit"
+                                        value="Регистрация"
+                                    />
+                                </form>
+                                <p className="auth_form-offer">
+                                    <span>Уже есть аккаунт? </span>
+                                    <button className="btn" onClick={login}>
+                                        {" "}
                                     Войти
                                 </button>
-                            </p>
-                        </div>
+                                </p>
+                            </div>}
                     </div>
                 </div>
             </div>
