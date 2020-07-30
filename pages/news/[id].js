52bb159af532bd050b9e04fa98b258b5b5f12be7
Doniyor <oripovd1@gmail.com>
2020-07-30 09:36:04 +0500

    add meta tags for new single

--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,22 +1,25 @@
 import SEO from "../../components/seo";
 import Footer from "../../components/footer";
 import { fetchMultipleUrls } from "../../libs/fetchMultipleUrls";
+import { getLocaleDate } from '../../libs/getLocaleDate'
 
 export default function NewSingle({ data }) {
     console.log(data)
     return (
         <>
-            <SEO />
+            <SEO 
+                title={data.new.meta.title || data.new.title}
+                description={data.new.meta.description || data.new.description.replace(/(<([^>]+)>)/gi, "")}
+                image={data.new.imageURL}
+                keywords={data.new.meta.tags}
+            />
             <section className="section_container">
                 <article className="delivery_content">
                     <div className="img_wrapper">
-                        <img src="../images/carousel_4.jpg" alt="Новости" />
+                        <img src={data.new.imageURL} alt={data.new.title} />
                     </div>
-                    <p className="date">13 июня</p>
-                    <h2>Описание и условия акции</h2>
-                    <p>Samsung переосмыслила традиционную роль телевизора в гостиной. Буквально недавно это был огромный прямоугольник, зияющий своей черной пустотой на всю комнату. Сегодня телевизор The Frame украшает интерьер, отображая в специальном режиме «Картина» лучшие произведения живописи и фотографии.</p>
-                    <h2>Искусство и телевизор</h2>
-                    <p>Благодаря сменным рамкам (черного, белого, бежевого цветов и под орех) The Frame органично вписывается в любой интерьер. Специальный кронштейн позволяет разместить телевизор максимально плотно к стене. В таком случае он растворяется среди висящих картин или фотографий. Однако при желании можно пойти еще дальше и докупить специальную подставку-мольберт.</p>
+                    <p className="date">{getLocaleDate(data.new.updated_at)}</p>
+                    <div dangerouslySetInnerHTML={{__html: data.new.full_text}} />
                 </article>
             </section>
             <Footer />
@@ -28,9 +31,7 @@ export async function getServerSideProps({ params, req }) {
 
     const urls = [`${process.env.NEWS_API_URL}/${params.id}?lang=${req.i18n.language}`];
 
-    const [{ data }] = await fetchMultipleUrls(urls);
-
-    console.log(urls)
+    const [data] = await fetchMultipleUrls(urls);
 
     return {
         props: { data },
