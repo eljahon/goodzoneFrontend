73a8c0163e4cb438de28bcb0e188e5fc8a54011f
salohiddin <saloh224@gmail.com>
2020-08-27 15:52:35 +0500

    fix: register and login

--- components/login-modal.js
+++ components/login-modal.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { FaTimes } from "react-icons/fa";
 import { useForm } from "react-hook-form";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import axios from "axios";
 import { setLocalStorage } from "../libs/localStorage";
 import swal from "sweetalert";
@@ -9,177 +9,167 @@ import { setUser } from "../redux/actions/authActions/authActions";
 import { useDispatch } from "react-redux";
 
 export default function LoginModal({ closeModal, goRegister, goCheckout }) {
-    const dispatch = useDispatch();
-    // changed register to goRegister because I used register from useForm (react-hook-form)
-    // to avoid duplicate variables
+  const dispatch = useDispatch();
+  // changed register to goRegister because I used register from useForm (react-hook-form)
+  // to avoid duplicate variables
 
-    const router = useRouter();
-    const [load, setLoad] = useState(false);
-    const [resetPassword, setResetPassword] = useState(false);
-    useEffect(() => {
-        setLoad(true);
-        document.body.classList.add("overflow");
-        return () => {
-            setLoad(false);
-            document.body.classList.remove("overflow");
-        };
-    });
-
-    const { register, handleSubmit, errors } = useForm();
-
-    const onSubmit = async (data) => {
-        try {
-            const response = await axios.post(process.env.LOGIN_API_URL, {
-                password: data.password,
-                phone: data.phoneNumber,
-            });
-
-            const {
-                data: { access_token },
-            } = response;
-
-            if (response.status === 200) {
-                setLocalStorage("access_token", access_token);
-                dispatch(setUser(response.data));
-                if (goCheckout)
-                    router.push('/checkout')
-                closeModal();
-            }
-        } catch (error) {
-            swal(error.response.data.Error.Message);
-        }
+  const router = useRouter();
+  const [load, setLoad] = useState(false);
+  const [resetPassword, setResetPassword] = useState(false);
+  useEffect(() => {
+    setLoad(true);
+    document.body.classList.add("overflow");
+    return () => {
+      setLoad(false);
+      document.body.classList.remove("overflow");
     };
+  });
 
-    const wrapperRef = useRef(null);
-    useOutsideCloseMenu(wrapperRef);
+  const { register, handleSubmit, errors } = useForm();
 
-    function useOutsideCloseMenu(ref) {
-        useEffect(() => {
-            function handleClickOutside(event) {
-                if (ref.current && !ref.current.contains(event.target)) {
-                    closeModal();
-                }
-            }
-            document.addEventListener("mousedown", handleClickOutside);
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        `https://cors-anywhere.herokuapp.com/`,
+        process.env.LOGIN_API_URL,
+        {
+          password: data.password,
+          phone: data.phoneNumber,
+        }
+      );
 
-            return () => {
-                document.removeEventListener("mousedown", handleClickOutside);
-            };
+      const {
+        data: { access_token },
+      } = response;
 
-        }, [ref]);
+      if (response.status === 200) {
+        setLocalStorage("access_token", access_token);
+        dispatch(setUser(response.data));
+        if (goCheckout) router.push("/checkout");
+        closeModal();
+      }
+    } catch (error) {
+      swal(error.response.data.Error.Message);
     }
+  };
 
-    return (
-        <div className="login_modal-wrapper">
-            <button className="btn close_btn" onClick={closeModal}>
-                <FaTimes />
-            </button>
-            <div className={`login_modal-holder ${load ? "show" : ""}`} ref={wrapperRef}>
-                <button className="btn close_btn" onClick={closeModal}>
-                    <FaTimes />
-                </button>
-                <div className="inner_block">
-                    <div className="auth_form">
-                        {resetPassword ? (
-                            <div className="auth_form-container">
-                                <h3>Забыли пароль</h3>
-                                <span className="sub_heading">
-                                    Мы вышлем вам код для сброса пароля
-                                </span>
-                                <form>
-                                    <input
-                                        type="tel"
-                                        name="phone_number"
-                                        placeholder="Номер телефона"
-                                        required
-                                    />
-                                    <button
-                                        type="submit"
-                                        className="btn btn_submit"
-                                    >
-                                        Сброс пароля
-                                    </button>
-                                </form>
-                                <p className="auth_form-offer">
-                                    <span>Вернуться к </span>
-                                    <button
-                                        className="btn"
-                                        onClick={() => setResetPassword(false)}
-                                    >
-                                        {" "}
-                                        Авторизации
-                                    </button>
-                                </p>
-                            </div>
-                        ) : (
-                                <>
-                                    <div className="auth_form-container">
-                                        <h3>Добро пожаловать</h3>
-                                        <span className="sub_heading">
-                                            Войдите с вашим номером телефона и
-                                            паролем
-                                    </span>
-                                        <form onSubmit={handleSubmit(onSubmit)}>
-                                            <input
-                                                ref={register({
-                                                    maxLength: 13,
-                                                    minLength: 13,
-                                                })}
-                                                type="tel"
-                                                name="phoneNumber"
-                                                defaultValue="+998"
-                                                required
-                                            />
-                                            {errors.phoneNumber && (
-                                                <p>
-                                                    Phone number should be 13
-                                                    characters long
-                                                </p>
-                                            )}
-                                            <input
-                                                ref={register}
-                                                type="password"
-                                                name="password"
-                                                placeholder="Пароль"
-                                                required
-                                            />
-                                            <button
-                                                type="submit"
-                                                className="btn btn_submit"
-                                            >
-                                                Войти
-                                            </button>
-                                        </form>
-                                        <p className="auth_form-offer">
-                                            <span>У вас нет аккаунта? </span>
-                                            <button
-                                                className="btn"
-                                                onClick={goRegister}
-                                            >
-                                                {" "}
-                                            Регистрация
-                                        </button>
-                                        </p>
-                                    </div>
-                                    <div className="auth_form-offer-section">
-                                        <p className="auth_form-offer">
-                                            <span>Забыли пароль? </span>
-                                            <button
-                                                className="btn"
-                                                onClick={() =>
-                                                    setResetPassword(true)
-                                                }
-                                            >
-                                                {" "}
-                                            Сбросить
-                                        </button>
-                                        </p>
-                                    </div>
-                                </>
-                            )}
-                    </div>
+  const wrapperRef = useRef(null);
+  useOutsideCloseMenu(wrapperRef);
+
+  function useOutsideCloseMenu(ref) {
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          closeModal();
+        }
+      }
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  return (
+    <div className="login_modal-wrapper">
+      <button className="btn close_btn" onClick={closeModal}>
+        <FaTimes />
+      </button>
+      <div
+        className={`login_modal-holder ${load ? "show" : ""}`}
+        ref={wrapperRef}
+      >
+        <button className="btn close_btn" onClick={closeModal}>
+          <FaTimes />
+        </button>
+        <div className="inner_block">
+          <div className="auth_form">
+            {resetPassword ? (
+              <div className="auth_form-container">
+                <h3>Забыли пароль</h3>
+                <span className="sub_heading">
+                  Мы вышлем вам код для сброса пароля
+                </span>
+                <form>
+                  <input
+                    type="tel"
+                    name="phone_number"
+                    placeholder="Номер телефона"
+                    required
+                  />
+                  <button type="submit" className="btn btn_submit">
+                    Сброс пароля
+                  </button>
+                </form>
+                <p className="auth_form-offer">
+                  <span>Вернуться к </span>
+                  <button
+                    className="btn"
+                    onClick={() => setResetPassword(false)}
+                  >
+                    {" "}
+                    Авторизации
+                  </button>
+                </p>
+              </div>
+            ) : (
+              <>
+                <div className="auth_form-container">
+                  <h3>Добро пожаловать</h3>
+                  <span className="sub_heading">
+                    Войдите с вашим номером телефона и паролем
+                  </span>
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                      ref={register({
+                        maxLength: 13,
+                        minLength: 13,
+                      })}
+                      type="tel"
+                      name="phoneNumber"
+                      defaultValue="+998"
+                      required
+                    />
+                    {errors.phoneNumber && (
+                      <p>Phone number should be 13 characters long</p>
+                    )}
+                    <input
+                      ref={register}
+                      type="password"
+                      name="password"
+                      placeholder="Пароль"
+                      required
+                    />
+                    <button type="submit" className="btn btn_submit">
+                      Войти
+                    </button>
+                  </form>
+                  <p className="auth_form-offer">
+                    <span>У вас нет аккаунта? </span>
+                    <button className="btn" onClick={goRegister}>
+                      {" "}
+                      Регистрация
+                    </button>
+                  </p>
                 </div>
-            </div>
+                <div className="auth_form-offer-section">
+                  <p className="auth_form-offer">
+                    <span>Забыли пароль? </span>
+                    <button
+                      className="btn"
+                      onClick={() => setResetPassword(true)}
+                    >
+                      {" "}
+                      Сбросить
+                    </button>
+                  </p>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
