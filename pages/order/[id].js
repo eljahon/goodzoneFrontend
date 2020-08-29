599de73fceb9b57c0a4190a9be34138edbfaf0e6
Khotam <bakhromov17@gmail.com>
2020-08-29 13:54:42 +0500

    fix order/[id].js select menu disabled when unired

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -1,74 +1,80 @@
 import SEO from "../../components/seo";
 import Footer from "../../components/footer";
 import { useState } from "react";
-import { withTranslation, Link } from '../../i18n'
+import { withTranslation, Link } from "../../i18n";
 import { fetchMultipleUrls } from "../../libs/fetchMultipleUrls";
 import { numberToPrice } from "../../libs/numberToPrice";
 
 function OrderDetails({ order, t }) {
-    const [payment, setPayment] = useState("click");
+    const [payment, setPayment] = useState(() => order.payment_method);
     const handleChange = (e) => {
         setPayment(e.target.value);
-    }
+    };
 
-    const prices = order.items.map(item => {
-        return item.price * item.quantity
-    })
+    const prices = order.items.map((item) => {
+        return item.price * item.quantity;
+    });
     const totalPrice = prices.reduce((a, b) => {
-        return Math.round(a + b)
-    })
+        return Math.round(a + b);
+    });
 
     const handleSubmit = async () => {
         window.location.href = `${process.env.PAYMENT_API_URL}?payment=${payment}&order_id=${order.number}&secret_key=b52ca358473ddbbc3a3a3cf374fc4f0c&amount=${totalPrice}`;
-        console.log('submit')
-    }
+        console.log("submit");
+    };
     return (
         <>
-            <SEO title={t('application-accepted')} />
+            <SEO title={t("application-accepted")} />
 
             <div className="order_received-wrapper">
                 <div className="order_received-container">
                     <Link href="/">
-                        <a className="btn home_btn">{t('back-to-home')}</a>
+                        <a className="btn home_btn">{t("back-to-home")}</a>
                     </Link>
                     <div className="order_info">
-                        <h2>{t('application-accepted')}</h2>
-                        <p>{t('thanks')}</p>
+                        <h2>{t("application-accepted")}</h2>
+                        <p>{t("thanks")}</p>
                         <div className="info_block-wrapper">
                             <div className="info_block">
-                                <p className="title">{t('order-number')}</p>
+                                <p className="title">{t("order-number")}</p>
                                 <p>{order.number}</p>
                             </div>
                             <div className="info_block">
-                                <p className="title">{t('date')}</p>
+                                <p className="title">{t("date")}</p>
                                 <p>{order.created_at}</p>
                             </div>
                             <div className="info_block">
-                                <p className="title">{t('total-amount')}</p>
+                                <p className="title">{t("total-amount")}</p>
                                 <p>{numberToPrice(totalPrice)}</p>
                             </div>
                             <div className="info_block">
-                                <p className="title">{t('payment-method')}</p>
+                                <p className="title">{t("payment-method")}</p>
                                 <p>{t(`${order.payment_method}`)}</p>
                             </div>
                         </div>
                     </div>
                     <div className="order_info">
-                        <h2>{t('order-info')}</h2>
-                        {order.items.map(item => (
+                        <h2>{t("order-info")}</h2>
+                        {order.items.map((item) => (
                             <div className="list_item" key={item.product_id}>
                                 <div className="list_title">
-                                    <p>{item.product_name} x {item.quantity}</p>
+                                    <p>
+                                        {item.product_name} x {item.quantity}
+                                    </p>
                                 </div>
                                 <div className="list_desc">
-                                    <p>{numberToPrice(item.price * item.quantity)}</p>
+                                    <p>
+                                        {numberToPrice(
+                                            item.price * item.quantity
+                                        )}
+                                    </p>
                                 </div>
                             </div>
                         ))}
 
                         <div className="list_item">
                             <div className="list_title">
-                                <p>{t('delivery-method')}</p>
+                                <p>{t("delivery-method")}</p>
                             </div>
                             <div className="list_desc">
                                 <p>{t(`${order.delivery_method}`)}</p>
@@ -76,7 +82,7 @@ function OrderDetails({ order, t }) {
                         </div>
                         <div className="list_item">
                             <div className="list_title">
-                                <p>{t('payment-method')}</p>
+                                <p>{t("payment-method")}</p>
                             </div>
                             <div className="list_desc">
                                 <p>{t(`${order.payment_method}`)}</p>
@@ -84,7 +90,7 @@ function OrderDetails({ order, t }) {
                         </div>
                         <div className="list_item">
                             <div className="list_title">
-                                <p>{t('total-amount')}</p>
+                                <p>{t("total-amount")}</p>
                             </div>
                             <div className="list_desc">
                                 <p>{numberToPrice(totalPrice)}</p>
@@ -92,19 +98,39 @@ function OrderDetails({ order, t }) {
                         </div>
                         <div className="list_item pay_now">
                             <div className="list_title">
-                                <p>{t('pay-now')}:</p>
+                                <p>{t("pay-now")}:</p>
                             </div>
                             <div className="list_desc">
-                                <select name="payment" id="payment" defaultValue="click" onChange={(e) => handleChange(e)}>
-                                    <option value="cash">{t('cash')}</option>
-                                    <option value="terminal">{t('terminal')}</option>
+                                <select
+                                    disabled={payment === "unired"}
+                                    name="payment"
+                                    id="payment"
+                                    defaultValue="click"
+                                    onChange={(e) => handleChange(e)}
+                                >
+                                    <option value="cash">{t("cash")}</option>
+                                    <option value="terminal">
+                                        {t("terminal")}
+                                    </option>
                                     <option value="click">Click</option>
                                     <option value="payme">Payme</option>
-                                    <option value="unired">Unired</option>
+                                    <option
+                                        disabled={payment !== "unired"}
+                                        value="unired"
+                                    >
+                                        Unired
+                                    </option>
                                 </select>
-                                {(payment == "click" || payment == "payme") ?
-                                    <button className="btn btn_pay" onClick={() => handleSubmit()}>{t('pay-now')}</button>
-                                    : ''}
+                                {payment == "click" || payment == "payme" ? (
+                                    <button
+                                        className="btn btn_pay"
+                                        onClick={() => handleSubmit()}
+                                    >
+                                        {t("pay-now")}
+                                    </button>
+                                ) : (
+                                    ""
+                                )}
                             </div>
                         </div>
                     </div>
@@ -116,10 +142,12 @@ function OrderDetails({ order, t }) {
     );
 }
 
-export default withTranslation('checkout')(OrderDetails)
+export default withTranslation("checkout")(OrderDetails);
 
 export async function getServerSideProps({ params, req }) {
-    const urls = [`${process.env.ORDER_API_URL}/${params.id}?lang=${req.i18n.language}`];
+    const urls = [
+        `${process.env.ORDER_API_URL}/${params.id}?lang=${req.i18n.language}`,
+    ];
 
     const [order] = await fetchMultipleUrls(urls);
 
