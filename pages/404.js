9c37a81e3ba4e973d1f4a16dbde62fcec90299c6
Пользователь <you@example.com>
2020-09-19 17:20:55 +0500

    added translation error page

--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,22 @@
-export default () => <div className="container-fluid bg-danger notfound_not d-flex align-items-center">
-    <div>
-        <div className="row notfound">
-            <div className="notfound-404 col-md-12">
-                <h1 className="text-center">404</h1>
-            </div>
-            <div className="col-md-12 mt-4 notfound_text">
-                <h2 className="text-center text-white">We are sorry, but the page you requested was not found</h2>
-            </div>
-            <div className="col-md-12 text-center mt-5 notfound_home">
-                <a href="/" className="btn  btn-outline-light ">Go Home</a>
+import { withTranslation } from "../i18n";
+
+function Page404({ t }) {
+    return (<div className="container-fluid bg-danger notfound_not d-flex align-items-center justify-content-center">
+        <div>
+            <div className="row notfound">
+                <div className="notfound-404 col-md-12">
+                    <h1 className="text-center">404</h1>
+                </div>
+                <div className="col-md-12 mt-4 notfound_text">
+                    <h2 className="text-center text-white">{t("not-found-text")}</h2>
+                </div>
+                <div className="col-md-12 text-center mt-5 notfound_home">
+                    <a href="/" className="btn  btn-outline-light ">{t("go-home")}</a>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+    </div>)
+}
+
+export default withTranslation("common")(Page404);
 
