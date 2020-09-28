dd619aa99ec7937d600e790ebb8c557d45fa89ee
Khotam <bakhromov17@gmail.com>
2020-09-28 16:45:56 +0500

    change: next.config.js, connect exchange return repair page to API

--- next.config.js
+++ next.config.js
@@ -1,37 +1,38 @@
 const { nextI18NextRewrites } = require("next-i18next/rewrites");
 const localeSubpaths = {
-  uz: "uz",
+    uz: "uz",
 };
 
 module.exports = {
-  env: {
-    PRODUCT_API_URL: "https://dev.goodzone.uz/v1/product",
-    CATEGORY_API_URL: "https://dev.goodzone.uz/v1/category",
-    BRAND_API_URL: "https://dev.goodzone.uz/v1/brand",
-    REGISTER_API_URL: "https://dev.goodzone.uz/v1/customer/register",
-    LOGIN_API_URL: "https://dev.goodzone.uz/v1/customer/login",
-    CHECK_USER_API_URL: "https://dev.goodzone.uz/v1/customer/exists",
-    AUTHORIZE_API_URL: "https://dev.goodzone.uz/v1/customer",
-    RESER_PASSWORD_API_URL: "https://dev.goodzone.uz/v1/customer/send-code",
-    CHECK_CODE_API_URL: "https://dev.goodzone.uz/v1/customer/check-code",
-    CHANGE_PASSWORD_API_URL: "https://dev.goodzone.uz/v1/customer/profile",
-    ORDER_API_URL: "https://dev.goodzone.uz/v1/order",
-    PAYMENT_API_URL: "https://pay.goodzone.uz/redirectPayment",
-    NEWS_API_URL: "https://dev.goodzone.uz/v1/new",
-    PROMO_API_URL: "https://dev.goodzone.uz/v1/promo",
-    MY_ORDERS_API_URL: "https://dev.goodzone.uz/v1/my-orders",
-    SHOPS_API_URL: "https://dev.goodzone.uz/v1/shop",
-    BANNER_API_URL: "https://dev.goodzone.uz/v1/banner",
-    PROFILE_API_URL: "https://dev.goodzone.uz/v1/customer/profile",
-    PRODUCT_FILTER_API_URL: "https://dev.goodzone.uz/v1/product/filter",
-    FEEDBACK_API_URL: "https://dev.goodzone.uz/v1/feedback",
-  },
-  publicRuntimeConfig: {
-    localeSubpaths,
-  },
-  experimental: {
-    async rewrites() {
-      return [...nextI18NextRewrites(localeSubpaths)];
+    env: {
+        PRODUCT_API_URL: "https://dev.goodzone.uz/v1/product",
+        CATEGORY_API_URL: "https://dev.goodzone.uz/v1/category",
+        BRAND_API_URL: "https://dev.goodzone.uz/v1/brand",
+        REGISTER_API_URL: "https://dev.goodzone.uz/v1/customer/register",
+        LOGIN_API_URL: "https://dev.goodzone.uz/v1/customer/login",
+        CHECK_USER_API_URL: "https://dev.goodzone.uz/v1/customer/exists",
+        AUTHORIZE_API_URL: "https://dev.goodzone.uz/v1/customer",
+        RESER_PASSWORD_API_URL: "https://dev.goodzone.uz/v1/customer/send-code",
+        CHECK_CODE_API_URL: "https://dev.goodzone.uz/v1/customer/check-code",
+        CHANGE_PASSWORD_API_URL: "https://dev.goodzone.uz/v1/customer/profile",
+        ORDER_API_URL: "https://dev.goodzone.uz/v1/order",
+        PAYMENT_API_URL: "https://pay.goodzone.uz/redirectPayment",
+        NEWS_API_URL: "https://dev.goodzone.uz/v1/new",
+        PROMO_API_URL: "https://dev.goodzone.uz/v1/promo",
+        MY_ORDERS_API_URL: "https://dev.goodzone.uz/v1/my-orders",
+        SHOPS_API_URL: "https://dev.goodzone.uz/v1/shop",
+        BANNER_API_URL: "https://dev.goodzone.uz/v1/banner",
+        PROFILE_API_URL: "https://dev.goodzone.uz/v1/customer/profile",
+        PRODUCT_FILTER_API_URL: "https://dev.goodzone.uz/v1/product/filter",
+        FEEDBACK_API_URL: "https://dev.goodzone.uz/v1/feedback",
+        PAGE_API_URL: "https://dev.goodzone.uz/v1/page",
     },
-  },
+    publicRuntimeConfig: {
+        localeSubpaths,
+    },
+    experimental: {
+        async rewrites() {
+            return [...nextI18NextRewrites(localeSubpaths)];
+        },
+    },
 };
--- pages/delivery.js
+++ pages/delivery.js
@@ -4,45 +4,52 @@ import { useEffect, useState } from "react";
 import { LazyImage } from "../components/lazy-image";
 import axios from "axios";
 
-
-
 export default function Delivery() {
     const [deliveryInfo, setDeliveryInfo] = useState(null);
     useEffect(() => {
         axios
-          .get(
-            'https://dev.goodzone.uz/v1/page/dostavka'
-          )
-          .then((response) => {
-            setDeliveryInfo(response.data.page);
-          })
-          .catch((error) => {
-            console.log("error " + error);
-          });
+            .get(`${process.env.PAGE_API_URL}/dostavka`)
+            .then((response) => {
+                setDeliveryInfo(response.data.page);
+            })
+            .catch((error) => {
+                console.log("error " + error);
+            });
     }, []);
-    
-    return deliveryInfo && (
-        <>
-             <SEO 
-                title={deliveryInfo.meta.title || deliveryInfo.title}
-                description={deliveryInfo.meta.description || deliveryInfo.description.replace(/(<([^>]+)>)/gi, "")}
-                keywords={deliveryInfo.meta.tags}
-            />
-            <div className="delivery_splash">
-                {deliveryInfo.preview_image ? <LazyImage
-                    src={deliveryInfo.preview_image}
-                    alt="Goodzone delivery"
-                /> : ""}
-            </div>
-            <section className="section_container">
-                <article className="delivery_content">
-                    <div className="content_block" dangerouslySetInnerHTML={{
-                            __html: deliveryInfo.content
-                    }}> 
-                    </div>
-                </article>
-            </section>
-            <Footer />
-        </>
+
+    return (
+        deliveryInfo && (
+            <>
+                <SEO
+                    title={deliveryInfo.meta.title || deliveryInfo.title}
+                    description={
+                        deliveryInfo.meta.description ||
+                        deliveryInfo.description.replace(/(<([^>]+)>)/gi, "")
+                    }
+                    keywords={deliveryInfo.meta.tags}
+                />
+                <div className="delivery_splash">
+                    {deliveryInfo.preview_image ? (
+                        <LazyImage
+                            src={deliveryInfo.preview_image}
+                            alt="Goodzone delivery"
+                        />
+                    ) : (
+                        ""
+                    )}
+                </div>
+                <section className="section_container">
+                    <article className="delivery_content">
+                        <div
+                            className="content_block"
+                            dangerouslySetInnerHTML={{
+                                __html: deliveryInfo.content,
+                            }}
+                        ></div>
+                    </article>
+                </section>
+                <Footer />
+            </>
+        )
     );
 }
