7d3615ba8501792d4cc64e9648d48a98f573f1ba
Khotam <bakhromov17@gmail.com>
2020-09-29 10:37:07 +0500

    refactor: create page component for static pages like delivery, payment-plans. connect payment-plans page to API

---
+++ components/page.js
@@ -0,0 +1,43 @@
+import SEO from "../components/seo";
+import Footer from "../components/footer";
+import { i18n } from "../i18n";
+import useAxios from "../libs/hooks/useAxios";
+
+export default function Page({ slug, fallbackImg }) {
+    const [pageInfo, error] = useAxios(
+        `${process.env.PAGE_API_URL}/${slug}?lang=${i18n.language}`
+    );
+
+    console.log("pageInfo :>> ", pageInfo);
+
+    const { content, meta, title, preview_image, description } = pageInfo
+        ? pageInfo?.data?.page
+        : {};
+
+    return (
+        pageInfo && (
+            <>
+                <SEO
+                    title={meta.title || title}
+                    description={
+                        meta.description ||
+                        description.replace(/(<([^>]+)>)/gi, "")
+                    }
+                    keywords={meta.tags}
+                />
+                <div className="delivery_splash">
+                    {preview_image && <img src={preview_image} alt={title} />}
+                </div>
+                <section className="section_container">
+                    <article className="delivery_content">
+                        <div
+                            dangerouslySetInnerHTML={{ __html: content }}
+                            className="content_block"
+                        ></div>
+                    </article>
+                </section>
+                <Footer />
+            </>
+        )
+    );
+}
--- pages/exchange-return-repair.js
+++ pages/exchange-return-repair.js
@@ -1,46 +1,12 @@
 import SEO from "../components/seo";
-import Footer from "../components/footer";
-import { i18n } from "../i18n";
-import useAxios from "../libs/hooks/useAxios";
+import Page from "../components/page";
 
 export default function ExchangeReturnRepair() {
-    const [pageInfo, error] = useAxios(
-        `${process.env.PAGE_API_URL}/obmen-vozvrat-i-remont-tovara?lang=${i18n.language}`
-    );
-
-    console.log("pageInfo :>> ", pageInfo);
-
-    const { content, meta, title, image, description } = pageInfo
-        ? pageInfo?.data?.page
-        : {};
-
+    console.log("EXCHANGE :>> ");
     return (
-        pageInfo && (
-            <>
-                <SEO
-                    title={meta.title || title}
-                    description={
-                        meta.description ||
-                        description.replace(/(<([^>]+)>)/gi, "")
-                    }
-                    keywords={meta.tags}
-                />
-                <div className="delivery_splash">
-                    <img
-                        src={image || "images/exchange.jpg"}
-                        alt="Goodzone Questions"
-                    />
-                </div>
-                <section className="section_container">
-                    <article className="delivery_content">
-                        <div
-                            dangerouslySetInnerHTML={{ __html: content }}
-                            className="content_block"
-                        ></div>
-                    </article>
-                </section>
-                <Footer />
-            </>
-        )
+        <Page
+            slug="obmen-vozvrat-i-remont-tovara"
+            fallbackImg="images/exchange.jpg"
+        />
     );
 }
