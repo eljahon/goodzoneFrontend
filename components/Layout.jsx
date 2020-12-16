026efdf08618269dda233934650790d6aa4b7eb2
salohiddin <saloh224@gmail.com>
2020-12-16 16:10:01 +0500

    added snow

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,25 @@
-import React from "react";
-import withYM from "next-ym";
-import { Router } from "next/router";
+import React from 'react'
+import withYM from 'next-ym'
+import { Router } from 'next/router'
+import Snowfall from 'react-snowfall'
 
 const Layout = ({ children }) => {
-    return <>{children}</>;
-};
+  return (
+    <>
+      <Snowfall
+        color='rgb(238, 238, 238)'
+        style={{
+          background: '#fff',
+          position: 'fixed',
+          zIndex: 9999,
+          background: 'transparent',
+        }}
+        // Controls the number of snowflakes that are created (default 150)
+        snowflakeCount={80}
+      />
+      {children}
+    </>
+  )
+}
 
-export default withYM("67755550", Router)(Layout);
+export default withYM('67755550', Router)(Layout)
