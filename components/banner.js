7050c498719122eba51ba13d110fb5968e5ba518
salohiddin <saloh224@gmail.com>
2020-12-23 18:13:27 +0500

    added link to banner

--- components/banner.js
+++ components/banner.js
@@ -1,20 +1,26 @@
-import React from "react";
-import { Col } from "react-bootstrap";
-import useAxios from "../libs/hooks/useAxios";
+import React from 'react'
+import { Col } from 'react-bootstrap'
+import useAxios from '../libs/hooks/useAxios'
+import Link from 'next/link'
+import { i18n } from '../i18n'
 
 export default function Banner({ name, size }) {
   const [banner, error] = useAxios(
     `${process.env.BANNER_API_URL}?position=${name}&active=true`
-  );
+  )
 
   return (
     banner && (
-      <Col lg={size} className="banner">
-        <img
-          src={banner.data.banners[0].image}
-          alt={banner.data.banners[0].title}
-        />
+      <Col lg={size} className='banner'>
+        <Link href={`/goodcart-bonus`}>
+          <a className='product_image'>
+            <img
+              src={banner.data.banners[0].image}
+              alt={banner.data.banners[0].title}
+            />
+          </a>
+        </Link>
       </Col>
     )
-  );
+  )
 }
