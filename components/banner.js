5090b8c7358baac444e17f1b860a803bb40c568c
salohiddin <saloh224@gmail.com>
2020-12-24 12:18:25 +0500

    banner fix

--- components/banner.js
+++ components/banner.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Col } from 'react-bootstrap'
 import useAxios from '../libs/hooks/useAxios'
 import Link from 'next/link'
 import { i18n } from '../i18n'
+import { useRouter } from 'next/router'
 
 export default function Banner({ name, size }) {
   const [banner, error] = useAxios(
@@ -12,7 +13,9 @@ export default function Banner({ name, size }) {
   return (
     banner && (
       <Col lg={size} className='banner'>
-        <Link href={`/goodcart-bonus`}>
+        <Link
+          href={banner.data.banners[0].url.replace('https://goodzone.uz', '')}
+        >
           <a className='product_image'>
             <img
               src={banner.data.banners[0].image}
