3f96dba79e12853e516d63ffc923a2303f1b4847
salohiddin <saloh224@gmail.com>
2021-01-09 19:44:25 +0500

    fix menu

--- components/departments-menu.js
+++ components/departments-menu.js
@@ -1,51 +1,46 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-import {
-  FaTimes,
-  FaArrowDown,
-  FaArrowRight,
-  FaArrowLeft,
-} from "react-icons/fa";
-import { i18n } from "../i18n";
-import { useRouter } from "next/router";
-import { withTranslation } from "../i18n";
-import { useDispatch } from "react-redux";
-import { clearFilters } from "../redux/actions/filterActions/filterActions";
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { FaTimes, FaArrowDown, FaArrowRight, FaArrowLeft } from 'react-icons/fa'
+import { i18n } from '../i18n'
+import { useRouter } from 'next/router'
+import { withTranslation } from '../i18n'
+import { useDispatch } from 'react-redux'
+import { clearFilters } from '../redux/actions/filterActions/filterActions'
 
 function DepartmentsMenu({ categories, closeMenu, menu = false, t }) {
-  const vw = window.innerWidth;
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const hasDynamicRouting = router.query.id;
-  const [show, setShow] = useState(false);
+  const vw = window.innerWidth
+  const dispatch = useDispatch()
+  const router = useRouter()
+  const hasDynamicRouting = router.query.id
+  const [show, setShow] = useState(false)
   const handleClose = () => {
-    if (vw <= 850) closeMenu();
-  };
+    if (vw <= 850) closeMenu()
+  }
 
   return (
     <>
       <nav className={`departments_menu`}>
-        <div className="drawer_header">
-          <button className="btn btn_close" onClick={closeMenu}>
-            <span className="close_icon">
+        <div className='drawer_header'>
+          <button className='btn btn_close' onClick={closeMenu}>
+            <span className='close_icon'>
               <FaTimes />
             </span>
           </button>
-          <Link href={i18n.language === "ru" ? "/" : "/uz"}>
+          <Link href={i18n.language === 'ru' ? '/' : '/uz'}>
             <a onClick={() => closeMenu()}>
               <img
-                src={hasDynamicRouting ? "../logo.svg" : "/logo.svg"}
-                alt="Goodzone"
+                src={hasDynamicRouting ? '../logo.svg' : '/logo.svg'}
+                alt='Goodzone'
               />
             </a>
           </Link>
         </div>
-        <ul className="nav-list">
+        <ul className='nav-list'>
           <li>
-            <Link href={i18n.language === "ru" ? "/promo" : "/uz/promo"}>
+            <Link href={i18n.language === 'ru' ? '/promo' : '/uz/promo'}>
               <a>
-                <button className="btn btn_promo">
-                  <span>{t("promo")}</span>
+                <button className='btn btn_promo'>
+                  <span>{t('promo')}</span>
                 </button>
               </a>
             </Link>
@@ -54,8 +49,8 @@ function DepartmentsMenu({ categories, closeMenu, menu = false, t }) {
             return (
               <li key={item.id}>
                 <Link
-                  href={`${i18n.language === "ru" ? "" : "/uz"}/shop/[id]`}
-                  as={`${i18n.language === "ru" ? "" : "/uz"}/shop/${
+                  href={`${i18n.language === 'ru' ? '' : '/uz'}/shop/[id]`}
+                  as={`${i18n.language === 'ru' ? '' : '/uz'}/shop/${
                     item.slug
                   }`}
                 >
@@ -65,53 +60,53 @@ function DepartmentsMenu({ categories, closeMenu, menu = false, t }) {
                   </a>
                 </Link>
                 {item.children && (
-                  <ul className="sub-menu">
+                  <ul className='sub-menu'>
                     {item.children.map((child) => {
                       return (
                         <li key={child.id}>
                           <Link
                             href={`${
-                              i18n.language === "ru" ? "" : "/uz"
+                              i18n.language === 'ru' ? '' : '/uz'
                             }/shop/[id]`}
-                            as={`${i18n.language === "ru" ? "" : "/uz"}/shop/${
+                            as={`${i18n.language === 'ru' ? '' : '/uz'}/shop/${
                               child.slug
                             }`}
                           >
                             <a
                               onClick={(e) => {
                                 if (child.children) {
-                                  e.preventDefault();
+                                  e.preventDefault()
                                 } else {
-                                  handleClose();
-                                  dispatch(clearFilters());
+                                  handleClose()
+                                  dispatch(clearFilters())
                                 }
                               }}
                             >
                               {i >= categories.length - 2
                                 ? child.children && <FaArrowLeft />
-                                : ""}
+                                : ''}
                               <span>{child.name}</span>
                               {i < categories.length - 2
                                 ? child.children && <FaArrowRight />
-                                : ""}
+                                : ''}
                             </a>
                           </Link>
                           {child.children && (
-                            <ul className="sub-menu">
+                            <ul className='sub-menu'>
                               {child.children.map((sub) => (
                                 <li key={sub.id}>
                                   <Link
                                     href={`${
-                                      i18n.language === "ru" ? "" : "/uz"
+                                      i18n.language === 'ru' ? '' : '/uz'
                                     }/shop/[id]`}
                                     as={`${
-                                      i18n.language === "ru" ? "" : "/uz"
+                                      i18n.language === 'ru' ? '' : '/uz'
                                     }/shop/${sub.slug}`}
                                   >
                                     <a
                                       onClick={() => {
-                                        handleClose();
-                                        dispatch(clearFilters());
+                                        handleClose()
+                                        dispatch(clearFilters())
                                       }}
                                     >
                                       {sub.name}
@@ -122,17 +117,17 @@ function DepartmentsMenu({ categories, closeMenu, menu = false, t }) {
                             </ul>
                           )}
                         </li>
-                      );
+                      )
                     })}
                   </ul>
                 )}
               </li>
-            );
+            )
           })}
         </ul>
       </nav>
     </>
-  );
+  )
 }
 
-export default withTranslation("navigation")(DepartmentsMenu);
+export default withTranslation('navigation')(DepartmentsMenu)
--- components/header.js
+++ components/header.js
@@ -1,59 +1,59 @@
-import React, { useState, useRef, useEffect } from "react";
-import { FaSearch, FaSignInAlt } from "react-icons/fa";
-import DepartmentsMenu from "./departments-menu";
-import SearchModal from "./search-modal";
-import LoginModal from "./login-modal";
-import RegisterModal from "./register-modal";
-import ProfileMenu from "./profile-menu";
-import { useSelector, shallowEqual } from "react-redux";
-import SearchBar from "./search-bar";
-import { useRouter } from "next/router";
-import TopBar from "./top-bar";
-import Headroom from "react-headroom";
-import { Link, withTranslation } from "../i18n";
-import BottomBar from "./bottom-bar";
-import IconUser from "./icons/IconUser";
-import IconMenu from "./icons/IconMenu";
+import React, { useState, useRef, useEffect } from 'react'
+import { FaSearch, FaSignInAlt } from 'react-icons/fa'
+import DepartmentsMenu from './departments-menu'
+import SearchModal from './search-modal'
+import LoginModal from './login-modal'
+import RegisterModal from './register-modal'
+import ProfileMenu from './profile-menu'
+import { useSelector, shallowEqual } from 'react-redux'
+import SearchBar from './search-bar'
+import { useRouter } from 'next/router'
+import TopBar from './top-bar'
+import Headroom from 'react-headroom'
+import { Link, withTranslation } from '../i18n'
+import BottomBar from './bottom-bar'
+import IconUser from './icons/IconUser'
+import IconMenu from './icons/IconMenu'
 
 function Header({ categories, t }) {
-  const vw = typeof window !== "undefined" ? window.innerWidth : 0;
+  const vw = typeof window !== 'undefined' ? window.innerWidth : 0
 
-  const [menu, setMenu] = useState(false);
-  const [searchPopup, setSearchPopup] = useState(false);
-  const [loginPopup, setLoginPopup] = useState(false);
-  const [registerPopup, setRegisterPopup] = useState(false);
-  const [profilePopup, setProfilePopup] = useState(false);
+  const [menu, setMenu] = useState(false)
+  const [searchPopup, setSearchPopup] = useState(false)
+  const [loginPopup, setLoginPopup] = useState(false)
+  const [registerPopup, setRegisterPopup] = useState(false)
+  const [profilePopup, setProfilePopup] = useState(false)
 
   useEffect(() => {
-    vw <= 850 ? setMenu(false) : setMenu(true);
-  }, []);
+    vw <= 850 ? setMenu(false) : setMenu(true)
+  }, [])
 
   const switchPopup = () => {
-    setLoginPopup(!loginPopup);
-    setRegisterPopup(!registerPopup);
-  };
+    setLoginPopup(!loginPopup)
+    setRegisterPopup(!registerPopup)
+  }
 
-  const user = useSelector((state) => state.auth.user, shallowEqual);
+  const user = useSelector((state) => state.auth.user, shallowEqual)
 
-  const router = useRouter();
-  const hasDynamicRouting = router.query.id;
+  const router = useRouter()
+  const hasDynamicRouting = router.query.id
 
-  const wrapperRef = useRef(null);
-  useOutsideCloseMenu(wrapperRef);
+  const wrapperRef = useRef(null)
+  useOutsideCloseMenu(wrapperRef)
 
   function useOutsideCloseMenu(ref) {
     useEffect(() => {
       function handleClickOutside(event) {
         if (ref.current && !ref.current.contains(event.target)) {
-          setProfilePopup(false);
+          setProfilePopup(false)
         }
       }
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside)
 
       return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [ref]);
+        document.removeEventListener('mousedown', handleClickOutside)
+      }
+    }, [ref])
   }
 
   return (
@@ -67,36 +67,36 @@ function Header({ categories, t }) {
       />
       <Headroom>
         <header>
-          <div className="left_menu">
+          <div className='left_menu'>
             <button
-              className="btn hamburger_icon"
+              className='btn hamburger_icon'
               onClick={() => setMenu(true)}
             >
               <IconMenu />
             </button>
-            <Link href="/">
-              <a className="logo">
+            <Link href='/'>
+              <a className='logo'>
                 <img
-                  src={hasDynamicRouting ? "../logo.svg" : "/logo.svg"}
-                  alt="Goodzone"
+                  src={hasDynamicRouting ? '../logo.svg' : '/logo.svg'}
+                  alt='Goodzone'
                 />
               </a>
             </Link>
           </div>
           <SearchBar />
-          <div className="right_menu">
+          <div className='right_menu'>
             {user ? (
-              <div className="popover" ref={wrapperRef}>
-                <div className="popover_handler">
+              <div className='popover' ref={wrapperRef}>
+                <div className='popover_handler'>
                   <button
-                    className="btn join_btn"
+                    className='btn join_btn'
                     onClick={() => setProfilePopup(!profilePopup)}
                   >
                     <IconUser />
-                    <span className="btn-text">{t("personal-area")}</span>
+                    <span className='btn-text'>{t('personal-area')}</span>
                   </button>
-                  <Link href="/account">
-                    <a className="btn join_btn mobile">
+                  <Link href='/account'>
+                    <a className='btn join_btn mobile'>
                       <IconUser />
                     </a>
                   </Link>
@@ -104,25 +104,25 @@ function Header({ categories, t }) {
                 {profilePopup ? (
                   <ProfileMenu closeMenu={() => setProfilePopup(false)} />
                 ) : (
-                  ""
+                  ''
                 )}
               </div>
             ) : (
               <button
-                className="btn join_btn"
+                className='btn join_btn'
                 onClick={() => setLoginPopup(true)}
               >
-                <span className="join_icon">
+                <span className='join_icon'>
                   <FaSignInAlt />
                 </span>
-                <span className="btn-text">{t("login")}</span>
+                <span className='btn-text'>{t('login')}</span>
               </button>
             )}
             <button
-              className="btn search_btn mobile"
+              className='btn search_btn mobile'
               onClick={() => setSearchPopup(true)}
             >
-              <span className="search_icon">
+              <span className='search_icon'>
                 <FaSearch />
               </span>
             </button>
@@ -130,21 +130,23 @@ function Header({ categories, t }) {
         </header>
       </Headroom>
 
-      <div className="base_header" />
+      <div className='base_header' />
       <BottomBar openLoginMenu={() => setLoginPopup(true)} />
 
-      {
+      {menu ? (
         <DepartmentsMenu
           categories={categories}
           closeMenu={() => setMenu(false)}
           menu={menu}
         />
-      }
+      ) : (
+        ''
+      )}
 
       {searchPopup ? (
         <SearchModal closeModal={() => setSearchPopup(false)} />
       ) : (
-        ""
+        ''
       )}
       {loginPopup ? (
         <LoginModal
@@ -152,7 +154,7 @@ function Header({ categories, t }) {
           goRegister={() => switchPopup()}
         />
       ) : (
-        ""
+        ''
       )}
       {registerPopup ? (
         <RegisterModal
@@ -160,10 +162,10 @@ function Header({ categories, t }) {
           login={() => switchPopup()}
         />
       ) : (
-        ""
+        ''
       )}
     </>
-  );
+  )
 }
 
-export default withTranslation("common")(Header);
+export default withTranslation('common')(Header)
