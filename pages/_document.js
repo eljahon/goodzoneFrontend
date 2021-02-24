320ec06d7936faf8c3ca4b29e775ea422a362e73
salohiddin <saloh224@gmail.com>
2021-02-24 09:54:42 +0500

    added facebook pixel

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-import { GA_TRACKING_ID_GOODZONE, GA_TRACKING_ID_UDEVS } from "../libs/gtag";
+import { GA_TRACKING_ID_GOODZONE, GA_TRACKING_ID_UDEVS } from '../libs/gtag'
 
 export default class MyDocument extends Document {
   render() {
@@ -42,15 +42,39 @@ export default class MyDocument extends Document {
           />
           <script
             async
-            charSet="utf-8"
-            src="//cdn.embedly.com/widgets/platform.js"
+            charSet='utf-8'
+            src='//cdn.embedly.com/widgets/platform.js'
           ></script>
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+               fbq('init', '168805641518216'); 
+              fbq('track', 'PageView');`,
+            }}
+          />
+
+          <noscript>
+            <img
+              height='1'
+              width='1'
+              src='https://www.facebook.com/tr?id=168805641518216&ev=PageView
+&noscript=1'
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
