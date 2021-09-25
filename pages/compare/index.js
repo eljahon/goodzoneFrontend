69b0c3b3c4eec6c636a6310a15ae5dbd854c04f2
salohiddin <saloh224@gmail.com>
2021-09-25 21:29:34 +0500

    fix seo in compare page

--- pages/compare/index.js
+++ pages/compare/index.js
@@ -1,29 +1,31 @@
-import Footer from "../../components/footer";
-import { Container } from "react-bootstrap";
-import { fetchMultipleUrls } from "../../libs/fetchMultipleUrls";
-import CompareCard from "../../components/compare";
+import Footer from '../../components/footer'
+import { Container } from 'react-bootstrap'
+import { fetchMultipleUrls } from '../../libs/fetchMultipleUrls'
+import CompareCard from '../../components/compare'
+import SEO from '../../components/seo'
 
 export default function Compare(categories) {
   return (
     <>
-      <div className="compare_body">
-        <Container fluid className="compare_container">
+      <SEO />
+      <div className='compare_body'>
+        <Container fluid className='compare_container'>
           <CompareCard />
         </Container>
       </div>
       <Footer />
     </>
-  );
+  )
 }
 
 export async function getServerSideProps({ req }) {
-  const urls = [`${process.env.CATEGORY_API_URL}?lang=${req.i18n.language}`];
+  const urls = [`${process.env.CATEGORY_API_URL}?lang=${req.i18n.language}`]
 
-  const [categories] = await fetchMultipleUrls(urls);
+  const [categories] = await fetchMultipleUrls(urls)
 
   return {
     props: {
       categories,
     },
-  };
+  }
 }
